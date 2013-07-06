403fe5232abc514f3022c29b7d438ffce2d78a29
Ethan Wang <slmagicbox@hotmail.com>
2013-07-06 20:58:46 +0800

    create RemotingComm.ps1

---
+++ ActiveDirectory/GetADUserMemberOf.ps1
@@ -0,0 +1,12 @@
+﻿$cred = Get-Credential
+Enter-PSSession -ComputerName w8c-dc01 -Credential $cred
+
+Import-Module ActiveDirectory
+Get-Command -Module ActiveDirectory | Format-Wide -Property Name -Column 4
+New-ADOrganizationalUnit -Name MyOU -Path "DC=lab,DC=ew"
+New-ADUser -Name Test03 -Path "OU=MyOU,DC=lab,DC=ew"
+
+Get-ADUser -Filter * -searchbase "ou=MyOU,dc=lab,dc=ew" -Properties MemberOf | select Name,MemberOf | Format-Table -Wrap -AutoSize
+Get-ADUser -Filter * -searchbase "ou=MyOU,dc=lab,dc=ew" -Properties MemberOf | Export-Csv -Path "c:\temp\123.csv"
+
+Exit-PSSession
---
+++ ActiveDirectory/RemotingConn.ps1
@@ -0,0 +1,5 @@
+﻿Set-Item WSMan:\localhost\client\trustedhosts "*.ew.lab" -Force 
+Get-Item WSMan:\localhost\client\*
+
+$cred = Get-Credential
+Enter-PSSession -ComputerName ex13-dc01 -Credential $cred 
