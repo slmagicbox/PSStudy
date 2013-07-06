084ca5877ed984ab88ef3c858eaf210fdc6053ee
Ethan Wang <slmagicbox@hotmail.com>
2013-07-06 21:20:02 +0800

    new RemotingConn.ps1

---
+++ ActiveDirectory/RemotingConn.ps1
@@ -0,0 +1,7 @@
+﻿Set-Item WSMan:\localhost\client\trustedhosts * -Force 
+Set-Item WSMan:\localhost\client\trustedhosts *.ew.lab -Force
+
+Get-Item WSMan:\localhost\client\*
+
+$cred = Get-Credential
+Enter-PSSession -ComputerName ex13-dc01.ew.lab -Credential $cred
